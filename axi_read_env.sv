78fffec463fc0066b0bed3d9a4e51b4ef69329cb
syllogismtech <syllogism.tech.work@gmail.com>
2025-05-01 10:31:30 +0100

    Create axi_read_env.sv

---
+++ axi_read_env.sv
@@ -0,0 +1,25 @@
+class axi_read_env extends uvm_env;
+  axi_read_master_agent m_agt;
+  axi_read_slave_agent  s_agt;
+  axi_read_scoreboard   scb;
+
+  `uvm_component_utils(axi_read_env)
+
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);   
+    m_agt    = axi_read_master_agent::type_id::create("m_agt", this);
+    s_agt    = axi_read_slave_agent::type_id::create("s_agt", this);
+    scb = axi_read_scoreboard::type_id::create("scb", this);
+  endfunction
+
+  function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    m_agt.mon.m_ap.connect(scb.m_scb);
+    s_agt.mon.s_ap.connect(scb.m_scb);
+  endfunction
+  
+endclass
