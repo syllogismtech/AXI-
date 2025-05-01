db578990aa68b5158bfa584bc13278678c63e101
syllogismtech <syllogism.tech.work@gmail.com>
2025-05-01 12:05:42 +0100

    Update axi_read_master_transaction.sv

--- axi_read_master_transaction.sv
+++ axi_read_master_transaction.sv
@@ -4,7 +4,7 @@ class axi_read_master_transaction extends uvm_sequence_item;
   rand bit [7:0] arlen;
   rand bit [2:0] arsize;
   rand bit [1:0] arburst;
-  bit arready;
+  rand bit arready;
   bit arvalid;
   
   function new(string name ="axi_read_master_transaction");
