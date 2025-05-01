fd9c142feef905deaac2d58519ddb288a49391d8
syllogismtech <syllogism.tech.work@gmail.com>
2025-05-01 11:00:10 +0100

    Update axi_read_scoreboard.sv

--- axi_read_scoreboard.sv
+++ axi_read_scoreboard.sv
@@ -1,11 +1,12 @@
 class axi_read_scoreboard extends uvm_scoreboard;
+
   `uvm_component_utils(axi_read_scoreboard)
 
-  uvm_analysis_imp #(axi_read_item, axi_read_scoreboard) master_ap;
-  uvm_analysis_imp #(axi_read_item, axi_read_scoreboard) slave_ap;
+  uvm_analysis_imp#(axi_read_master_transaction, axi_read_scoreboard) m_scb;
+  uvm_analysis_imp#(axi_read_slave_transaction,  axi_read_scoreboard) s_scb;
 
-  axi_read_item master_q[$]; 
-  axi_read_item slave_q[$];  
+  axi_read_master_transaction master_q[$];
+  axi_read_slave_transaction  slave_q[$];
 
   function new(string name, uvm_component parent);
     super.new(name, parent);
@@ -13,21 +14,22 @@ class axi_read_scoreboard extends uvm_scoreboard;
     slave_ap  = new("slave_ap", this);
   endfunction
 
-  function void write(input axi_read_item t);
-    if (t.is_read_address) begin
-      master_q.push_back(t);
-    end
-    else begin
-      slave_q.push_back(t);
-    end
+  function void write(axi_read_master_transaction t);
+    master_q.push_back(t);
+  endfunction
+
+  function void write(axi_read_slave_transaction t);
+    slave_q.push_back(t);
   endfunction
 
   task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+
+    axi_read_master_transaction master_txn;
+    axi_read_slave_transaction  slave_txn;
+
     forever begin
       wait (master_q.size() > 0 && slave_q.size() > 0);
-
-      axi_read_item master_txn;
-      axi_read_item slave_txn;
 
       master_txn = master_q.pop_front();
       int burst_len = master_txn.arlen + 1;
@@ -36,14 +38,20 @@ class axi_read_scoreboard extends uvm_scoreboard;
         wait (slave_q.size() > 0);
         slave_txn = slave_q.pop_front();
 
-    
+        if (slave_txn.rid !== master_txn.arid) begin
+          `uvm_error("SCOREBOARD", $sformatf("ID mismatch: Expected ID = %0h, Got = %0h",
+                      master_txn.arid, slave_txn.rid))
+        end
+
         if (slave_txn.rdata !== master_txn.expected_rdata[i]) begin
-          `uvm_error("SCOREBOARD", $sformatf("Data mismatch at beat %0d: expected %0h, got %0h",
-                        i, master_txn.expected_rdata[i], slave_txn.rdata))
-        end else begin
-          `uvm_info("SCOREBOARD", $sformatf("Read matched at beat %0d: %0h", i, slave_txn.rdata), UVM_LOW)
+          `uvm_error("SCOREBOARD", $sformatf("Data mismatch at beat %0d: Expected = %0h, Got = %0h",
+                      i, master_txn.expected_rdata[i], slave_txn.rdata))
+        end
+        else begin
+          `uvm_info("SCOREBOARD", $sformatf("Beat %0d match: Data = %0h", i, slave_txn.rdata), UVM_LOW)
         end
       end
     end
   endtask
+
 endclass
