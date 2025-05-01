ff490a4d0b0b301519b8ecbd1e5b03ffc642a9cf
syllogismtech <syllogism.tech.work@gmail.com>
2025-05-01 11:03:29 +0100

    Create axi_read_master_transaction.sv

---
+++ axi_read_master_transaction.sv
@@ -0,0 +1,12 @@
+class axi_read_master_transaction extends uvm_sequence_item;
+  'uvm_object_utils(axi_read_master_transaction)
+  rand bit[31:0] araddr;
+  rand bit [7:0] arlen;
+  rand bit [2:0] arsize;
+  rand bit [1:0] arburst;
+  bit arready;
+  
+  function new(string name ="axi_read_master_transaction");
+    super.new(name);
+  endfunction
+endclass																								
