01b2a7b4bb7f43936a4decdd99660df89e6a41f5
syllogismtech <syllogism.tech.work@gmail.com>
2025-05-01 11:38:58 +0100

    Update axi_testbench.sv

--- axi_testbench.sv
+++ axi_testbench.sv
@@ -1,5 +1,21 @@
-  import uvm_pkg::*;
-  `include "uvm_macros.svh" 
+ import uvm_pkg::*;
+`include "uvm_macros.svh" 
+`include "axi_read_slave_transaction.sv"
+`include "axi_read_master_transaction.sv"
+`include "axi_read_master_seq.sv"
+`include "axi_read_slave_seq.sv"
+`include "axi_read_master_sequencer.sv"
+`include "axi_read_read_sequencer.sv"
+`include "axi_read_master_agent.sv"
+`include "axi_read_slave_agent.sv"
+`include "axi_read_master_driver.sv"
+`include "axi_read_slave_driver.sv"
+`include "axi_read_master_monitor.sv"
+`include "axi_read_slave_monitor.sv"
+`include "axi_read_env.sv"
+`include "axi_read_scoreboard.sv"
+`include "axi_read_test.sv"
+
 
 module testbench;
   read_interface in();  
@@ -18,6 +34,9 @@ module testbench;
 
   initial begin
     uvm_config_db#(virtual axi_read_if)::set(null, "*", "vif", in);
+  end
+  
+  initial begin
     run_test("axi_read_test");
   end
 
