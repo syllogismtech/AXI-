de26a868755a0e96ba66b6b8b25699d03d5c5ea6
syllogismtech <syllogism.tech.work@gmail.com>
2025-05-01 11:58:20 +0100

    Create read_interface.sv

---
+++ read_interface.sv
@@ -0,0 +1,18 @@
+interface read_interface #(parameter ADDR_WIDTH = 32, DATA_WIDTH = 64, ID_WIDTH = 4); 
+  logic ACLK;
+  logic ARESETn;
+
+  logic [ADDR_WIDTH-1:0]   ARADDR;
+  logic [7:0]              ARLEN;
+  logic [2:0]              ARSIZE;
+  logic [1:0]              ARBURST;
+  logic                    ARVALID;
+  logic                    ARREADY;
+
+  logic [DATA_WIDTH-1:0]   RDATA;
+  //logic [1:0]              RRESP;
+  logic                    RLAST;
+  logic                    RVALID;
+  logic                    RREADY;
+  
+endinterface
