038caf6b27ba68338c8670fea40d4fd32dea2028
syllogismtech <syllogism.tech.work@gmail.com>
2025-05-01 10:43:03 +0100

    Update axi_read_master_agent.sv

--- axi_read_master_agent.sv
+++ axi_read_master_agent.sv
@@ -1,7 +1,7 @@
 class axi_read_master_agent extends uvm_agent;
-  axi_read_slave_driver m_driv;
-  axi_read_slave_monitor m_mon; 
-  axi_read_slave_sequencer m_seqr;
+  axi_read_master_driver m_driv;
+  axi_read_master_monitor m_mon; 
+  axi_read_master_sequencer m_seqr;
 
   `uvm_component_utils(axi_read_master_agent)
 
@@ -10,14 +10,13 @@ class axi_read_master_agent extends uvm_agent;
   endfunction
 
   function void build_phase(uvm_phase phase);
-    m_driv  = axi_read_slave_driver::type_id::create("m_driv", this);
-    m_mon= axi_read_slave_monitor::type_id::create("m_mon", this);
-    m_seqr = axi_read_slave_sequencer::type_id::create("m_seqr", this);
+    m_driv  = axi_read_master_driver::type_id::create("m_driv", this);
+    m_mon = axi_read_master_monitor::type_id::create("m_mon", this);
+    m_seqr = axi_read_master_sequencer::type_id::create("m_seqr", this);
 
   endfunction
 
   function void connect_phase(uvm_phase phase);
-    super.connect_phase(phase);
     m_driv.seq_item_port.connect(m_mon.seq_item_export);
   endfunction
 endclass
