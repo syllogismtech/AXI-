8b2d80ba1d5e4f850cbaa697c931ddc5b6379072
syllogismtech <syllogism.tech.work@gmail.com>
2025-05-03 10:30:52 +0530

    Update axi_read_slave_driver.sv

--- axi_read_slave_driver.sv
+++ axi_read_slave_driver.sv
@@ -11,30 +11,38 @@ class axi_read_slave_driver extends uvm_driver #(axi_read_slave_transaction);
       `uvm_fatal("SLV_DRV", "Interface not found")
   endfunction
 
-  task run_phase(uvm_phase phase);
-    forever begin
-      
-      @(posedge vif.clk);
-      if (vif.arvalid && vif.arready) begin
-        int burst_len = vif.arlen;
-        bit [3:0] read_id;
-        read_id =vif.arid;
+task run_phase(uvm_phase phase);
+  forever begin
+    @(posedge vif.clk);
+    if (vif.arvalid && vif.arready) begin
+      int burst_len = vif.arlen;
+      bit [3:0] read_id;
+      read_id = vif.arid;
 
+      for (int i = 0; i <= burst_len; i++) begin
+        bit [63:0] data;
+        void'(std::randomize(data));
+
+        case (vif.arsize)
+          3'd0: vif.rdata <= data[7:0];
+          3'd1: vif.rdata <= data[15:0];
+          3'd2: vif.rdata <= data[31:0];
+          3'd3: vif.rdata <= data[63:0];
+          default: vif.rdata <= 64'hDEADBEEFDEADBEEF;
+        endcase
         
-        for (int i = 0; i <= burst_len; i++) begin
-          vif.rdata  <= vif.araddr+8;
-          vif.rvalid <= 1;
-          vif.rlast  <= (i == burst_len);
-          vif.rid <=read_id;
+        vif.rvalid <= 1;
+        vif.rlast  <= (i == burst_len);
+        vif.rid    <= read_id;
 
-          
-          @(posedge vif.clk);
-          while (!vif.rready) @(posedge vif.clk);
+        @(posedge vif.clk);
+        while (!vif.rready) @(posedge vif.clk);
 
-          vif.rvalid <= 0;
-          vif.rlast  <= 0;
-        end
+        vif.rvalid <= 0;
+        vif.rlast  <= 0;
       end
     end
-  endtask
+  end
+endtask
+
 endclass
